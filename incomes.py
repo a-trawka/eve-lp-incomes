36611e5233fd5d72ca9778df2afea5f41f85c960
Adrian T <ad.trawka@gmail.com>
2018-09-17 23:11:04 +0200

    General refactor. Slighty change problem approach solution. Add TODOs.

--- incomes.py
+++ incomes.py
@@ -1,19 +1,18 @@
 from csv import DictReader
 from operator import itemgetter
 
-from lxml import html
 from requests import get, post
 
 EVEPRAISAL_URL = 'http://evepraisal.com'
 
 
-def query_evepraisal(item_dicts, market):
+def query_evepraisal(store_data, market='jita'):
     """
-    :param item_dicts: list of item dicts
+    :param store_data: LP store data - list of dicts
     :param market: ex. jita
-    :return: item regional appraisal response
+    :return: response containing important appraisal id in headers
     """
-    items_text = '\n'.join(i['item'] for i in item_dicts)
+    items_text = '\n'.join(i['item'] for i in store_data)
     f = {
         'persist': (None, 'yes'),
         'price_percentage': (None, 100),
@@ -24,69 +23,66 @@ def query_evepraisal(item_dicts, market):
     return post(url=f'{EVEPRAISAL_URL}/appraisal', files=f)
 
 
-def appraisal(appraisal_id):
-    return get(f'{EVEPRAISAL_URL}/a/{appraisal_id}')
+def appraisal_data(appraisal_id):
+    return get(f'{EVEPRAISAL_URL}/a/{appraisal_id}.json')
 
 
-def evepraisal_prices(prices_response_content, item_dicts):
+def iter_formatted_items(data, store_data):
     """
-    :param prices_response_content: response content in bytes (ex. Response.content)
-    :param item_dicts: list of item dicts
-    :return: tuple of prices
+    :param data: appraisal data dict
+    :param store_data: LP store data - list of dicts
+    :return: generator of item dicts containing market data
     """
-    all_items_prices = []
-    tree = html.fromstring(prices_response_content)
-    tbody = tree.xpath('/html/body/div[1]/div[3]/div[2]/table/tbody')[0]  # table tbody containing prices
-    for row in tbody.xpath('tr'):
-        item_name = row.xpath('td[2]')[0].xpath("a[@class='text-light']/strong")[0].text.strip()
-        matching_item = next(  # find the matching item by name and then isk required to purchase the item
-            filter(
-                lambda item: item['item'] == item_name,
-                item_dicts
-            )
-        )
-        isk_cost = int(matching_item['isk'])
-        lp_cost = int(matching_item['lp'])
-        raw_prices_text = row.xpath('td[4]')[0].text_content().strip()
-        raw_prices = raw_prices_text.replace(' ', '').split('\n')
-        prices = map(  # both buy and sell prices
-            lambda p: int(p.split('.')[0].replace(',', '')),
-            raw_prices
-        )
-        all_items_prices.append([item_name, next(prices) - isk_cost, lp_cost])  # [item, price-isk cost=income, lp cost]
-    return all_items_prices
+    for item in data['items']:
+        name = item['name']
+        matching_store_item = next(filter(
+            lambda store_item: store_item['item'] == name,
+            store_data
+        ))
+        buy_scope = item['prices']['buy']
+        sell_scope = item['prices']['sell']
+        yield {
+            'item': matching_store_item,
+            'buy_price': buy_scope['min'],
+            'buy_orders': buy_scope['order_count'],
+            'sell_price': sell_scope['min'],
+            'sell_orders': sell_scope['order_count']
+        }
 
 
-def most_protifable_items(prices, n=5):
-    return sorted(prices, key=itemgetter(1), reverse=True)[:n]
-
-
-def items():
+def lp_store_items():
     with open('resources/concord_items.csv') as csvfile:
         return list(DictReader(csvfile))
 
 
-def prettify_results(results):
-    def commafy(n):
-        r = []
-        for i, c in enumerate(reversed(str(n))):
-            if i and (not (i % 3)):
-                r.insert(0, ',')
-            r.insert(0, c)
-        return ''.join(r)
-
-    return ((commafy(r[1]), commafy(r[2]), r[0]) for r in results)
+# def prettify_results(results):
+#     def commafy(n):
+#         r = []
+#         for i, c in enumerate(reversed(str(n))):
+#             if i and (not (i % 3)):
+#                 r.insert(0, ',')
+#             r.insert(0, c)
+#         return ''.join(r)
+#
+#     return ((commafy(r[1]), commafy(r[2]), r[0]) for r in results)
 
 
 if __name__ == '__main__':
     # TODO: add current amount of LPs
-    item_dicts = items()  # list of item dicts
+    # TODO: query more than 1 trade hubs
+    lp_store_data = lp_store_items()
 
-    post_response = query_evepraisal(item_dicts, 'jita')
+    post_response = query_evepraisal(lp_store_data)
     appraisal_id = post_response.headers['X-Appraisal-Id']
-    appraisal_response = appraisal(appraisal_id)
+    appraisal_response = appraisal_data(appraisal_id)
+    appraisal_data = appraisal_response.json()
 
-    prices = evepraisal_prices(appraisal_response.content, item_dicts)
-    results = most_protifable_items(prices)
-    for result in prettify_results(results):
-        print(result)
+    items = iter_formatted_items(appraisal_data, lp_store_data)
+    filtered_zero_orders = filter(
+        lambda item: item['buy_orders'] != 0 and item['sell_orders'] != 0,
+        items
+    )
+    for i in sorted(filtered_zero_orders,
+                    key=itemgetter('sell_price'),
+                    reverse=True)[:10]:
+        print(i)
